e41f8eed9071cdf68f877b190b133b49803d28f3
Phillip Harris <philliptharris@gmail.com>
2026-02-07 16:54:00 -0500

    Reformat Package.swift

--- Package.swift
+++ Package.swift
@@ -1,26 +1,14 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AutoLayoutWoot",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "AutoLayoutWoot",
-            targets: ["AutoLayoutWoot"]
-        ),
+        .library(name: "AutoLayoutWoot", targets: ["AutoLayoutWoot"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AutoLayoutWoot"
-        ),
-        .testTarget(
-            name: "AutoLayoutWootTests",
-            dependencies: ["AutoLayoutWoot"]
-        ),
+        .target(name: "AutoLayoutWoot"),
+        .testTarget(name: "AutoLayoutWootTests", dependencies: ["AutoLayoutWoot"]),
     ]
 )
